f2901bca5fa828d33f787c01b0c5c42421774e0f
Owen <youngwhenbold@outlook.com>
2024-11-11 19:20:29 +0800

    add contact

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,9 @@
+import Contact from "./components/aboutme/Contact";
 import Hero from "./components/aboutme/Hero";
+import KeyMetrics from "./components/aboutme/KeyMetrics";
 import LogoAnimation from "./components/aboutme/LogoAnimation";
 import Portfolio from "./components/aboutme/Portfolio";
+import Services from "./components/aboutme/Services";
 import Stack from "./components/aboutme/Stack";
 
 export default function Home() {
@@ -10,6 +13,9 @@ export default function Home() {
       <Stack />
       <LogoAnimation />
       <Portfolio />
+      <KeyMetrics />
+      <Services />
+      <Contact />
     </>
   );
 }
