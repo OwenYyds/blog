e490b501f443699c61eb73f92b54f567df0257b0
Owen <youngwhenbold@outlook.com>
2024-11-11 18:25:35 +0800

    add portfolio

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import Hero from "./components/aboutme/Hero";
 import LogoAnimation from "./components/aboutme/LogoAnimation";
+import Portfolio from "./components/aboutme/Portfolio";
 import Stack from "./components/aboutme/Stack";
 
 export default function Home() {
@@ -8,6 +9,7 @@ export default function Home() {
       <Hero />
       <Stack />
       <LogoAnimation />
+      <Portfolio />
     </>
   );
 }
