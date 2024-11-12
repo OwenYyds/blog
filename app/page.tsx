7ce00bd9ec84fc647c04983db2fd40fdb785ad53
Owen <youngwhenbold@outlook.com>
2024-11-12 17:45:06 +0800

    add scrollToTopButton with animations

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ import KeyMetrics from "./components/aboutme/KeyMetrics";
 import LogoAnimation from "./components/aboutme/LogoAnimation";
 import Navbar from "./components/aboutme/Navbar";
 import Portfolio from "./components/aboutme/Portfolio";
+import ScrollToTopButton from "./components/aboutme/ScrollToTopButton";
 import Services from "./components/aboutme/Services";
 import Stack from "./components/aboutme/Stack";
 
@@ -20,6 +21,7 @@ export default function Home() {
       <Services />
       <Contact />
       <Footer />
+      <ScrollToTopButton />
     </>
   );
 }
